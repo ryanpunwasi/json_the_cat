9072a43c0d61e0ddb1b9ba78d4f5a035d51761c4
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-09 14:09:19 -0400

    Implemented output of data and error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,20 @@
 const request = require("request");
-request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (err, resp, body) => {
-  if (!err && resp.statusCode === 200) {
-    console.log(JSON.parse(body)[0].description);
+const breed = process.argv.slice(2)[0];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, resp, body) => {
+  try {
+    if (!err) {
+      const data = JSON.parse(body);
+      if (data.length) {
+        console.log(data[0].description);
+      } else {
+        throw new Error('No breed with that name was found.');
+      }
+    }  else if (err) {
+      throw err;
+    }
+  } catch (e) {
+    console.log(`We were unable to complete your request. See details below:\n${e}`);
   }
+  
 });
