0d24c633620ab610b72a82d25adaa265b68467a3
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-09 13:30:44 -0400

    Implemented successful query of cat api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,6 @@
 const request = require("request");
+request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (err, resp, body) => {
+  if (!err && resp.statusCode === 200) {
+    console.log(JSON.parse(body)[0].description);
+  }
+});
